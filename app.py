07ec422c81a13eeeba57350269745f42c3d15eee
DileepSathya <dileepsathya2205@gmail.com>
2026-06-26 11:51:45 +0530

    port chnage commit

--- app.py
+++ app.py
@@ -203,5 +203,8 @@ def latest_license():
         return redirect(url_for("generate_license"))
 
 
+"""if __name__ == "__main__":
+    app.run(debug=True)"""
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
